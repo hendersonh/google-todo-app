58af978b73c6f2633c88e71800a6d7f9918d8d49
Henderson Hood <hendy@hendersonhood.com>
2026-04-26 20:24:27 -0400

    fix: mark browser wrapper as executable and add no-sandbox flag

--- .agent/chrome_wrapper.sh
+++ .agent/chrome_wrapper.sh
@@ -4,4 +4,4 @@ export XDG_RUNTIME_DIR=/run/user/1000
 # Only use headless if we are in a non-GUI environment, 
 # but the user seems to have a GUI. 
 # Antigravity will likely pass its own flags.
-exec /usr/local/bin/chrome --ozone-platform=wayland --enable-features=WaylandWindowDecorations "$@"
+exec /usr/local/bin/chrome --no-sandbox --ozone-platform=wayland --enable-features=WaylandWindowDecorations "$@"
